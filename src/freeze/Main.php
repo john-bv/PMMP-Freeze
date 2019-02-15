5ec39d3e7fde8c9c8a7cb8ae597a6de1627e6116
Olybear9 <olybear9@gmail.com>
2019-02-15 01:44:43 -0700

    Freeze 1.2.0
    Adds configuration for customizations

--- src/freeze/Main.php
+++ src/freeze/Main.php
@@ -19,15 +19,17 @@ use pocketmine\event\player\PlayerEvent;
 use pocketmine\level\Location;
 
 class Main extends PluginBase implements Listener {
-	public $frozen = array();
-	public $freeze;
+	private $frozen = array();
+	private $freeze;
+	private $freeze_tag;
 	public function onEnable() :void {
 		$this->getLogger()->info(TextFormat::GREEN . "Enabled Freeze. Made by: Bavfalcon9");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		//Initialize Config
 		$this->saveResource("config.yml");
 		$this->saveDefaultConfig();
-		$this->freeze = $this->getConfig()->("format");
+		$this->freeze = $this->getConfig()->get("format");
+		$this->freeze_tag = $this->getConfig()->get("format-tag");
 	}
 	public function onMove(PlayerMoveEvent $event) : void {
 		$player = $event->getPlayer();
@@ -43,18 +45,20 @@ class Main extends PluginBase implements Listener {
 		$entity = $event->getEntity();
 		if($entity instanceof Player) {
 			if(in_array($entity->getName(), $this->frozen)) {
-				if($this->getConfig()-get("attacked-frozen") == true) return;
-				$event->setCancelled(true);
-				if($damager instanceof Player) {
-					$damager->sendMessage($this->freeze . TextFormat::RED."You can't hit frozen Players.");
+				if($this->getConfig()->get("attacked-frozen") === false) {
+					$event->setCancelled(true);
+					if($damager instanceof Player) {
+						$damager->sendMessage($this->freeze . TextFormat::RED."You can't hit frozen Players.");
+					}
 				}
 			}
 		}
 		if($damager instanceof Player) {
 			if(in_array($damager->getName(), $this->frozen)) {
-				if($this->getConfig()-get("attack-frozen") == true) return;
-				$event->setCancelled(true);
+				if($this->getConfig()->get("attack-frozen") === false) {
+					$event->setCancelled(true);
 					$damager->sendMessage($this->freeze . TextFormat::RED."You can't hit players while frozen.");
+				}
 			}
 		}
 
@@ -63,6 +67,7 @@ class Main extends PluginBase implements Listener {
 		$player = $event->getPlayer();
 		if(in_array($player->getName(), $this->frozen)) {
 			$player->setImmobile(true);
+			if($this->getConfig()->get("frozen-tag") === true) $player->setNameTag($this->freeze_tag.$player->getNametag()); //fix bug with purechat/perms
 			$player->addActionBarMessage(TextFormat::RED . "You are Frozen!");
 			$player->sendMessage($this->freeze . TextFormat::RED . "You are frozen! Please listen to staff instruction to prevent a ban. §c§lDO NOT LOG OUT!§r§c ");
 		}
@@ -71,8 +76,10 @@ class Main extends PluginBase implements Listener {
 		$player = $e->getPlayer();
 		if(in_array($player->getName(), $this->frozen)) {
 			if(!$this->getConfig()->get("autoban")) return;
-			$this->getServer()->getNameBans()->addBan($player->getName(), "§cYou are banned for: §b[Auto] Logging out while frozen", null, "CONSOLE");
-			$this->getServer()->broadcastMessage($this->freeze . $player->getName() . "§f was §cbanned§f because they logged out while frozen.");
+			else {
+				$this->getServer()->getNameBans()->addBan($player->getName(), $this->filterVar('autoban-reason', $player), null, "CONSOLE");
+				$this->getServer()->broadcastMessage($this->freeze . $this->filterVar('autoban-msg', $player));
+			}
 		}
 	}
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
@@ -104,7 +111,7 @@ class Main extends PluginBase implements Listener {
 				array_push($this->frozen, $player);
 				$p->setImmobile(true);
 				$this->getServer()->broadcastMessage($this->freeze ."§e" . $player . "§r is now frozen.");
-				$p->setNameTag("§c§lFROZEN§r ".$p->getNametag());
+				if($this->getConfig()->get("frozen-tag") === true) $p->setNameTag($this->freeze_tag.$p->getNametag());
 				$p->addActionBarMessage(TextFormat::RED . "You are Frozen!");
 				$p->sendMessage($this->freeze . TextFormat::RED . "You are frozen! Please listen to staff instruction to prevent a ban. §c§lDO NOT LOG OUT!§r§c ");
 				return true;
@@ -145,7 +152,7 @@ class Main extends PluginBase implements Listener {
 				$p->sendMessage($this->freeze . TextFormat::GREEN."You are no longer frozen.");
 				$p->setImmobile(false);
 				$p->addTitle(TextFormat::GREEN . "You are unfrozen!");
-				$p->setNameTag(str_replace("§c§lFROZEN§r ", "", $p->getNameTag()));
+				$p->setNameTag(str_replace($this->freeze_tag, "", $p->getNameTag()));
 				$sender->sendMessage($this->freeze . "§e" . $player . "§r is no longer frozen.");
 				return true;		
 			} else {
@@ -156,6 +163,7 @@ class Main extends PluginBase implements Listener {
 		}
 		return true;
 	}
+	// Function being deprecated soon due to it's uselessness owo
 	public function findPlayer($username) {
 		$plyerFull = "";
 		foreach($this->getServer()->getOnlinePlayers() as $oplayer) {
@@ -178,16 +186,23 @@ class Main extends PluginBase implements Listener {
         if ($event->isCancelled()) return true;
 		$player = $event->getPlayer();
 		$message = $event->getMessage();
-		if($this->getConfig("commands-frozen")) return true;
-		if(strpos($message, "/") !== false) {
-			if(in_array($player->getName(), $this->frozen)) {
-				$player->addActionBarMessage(TextFormat::RED . "You are Frozen!");
-				$player->sendMessage($this->freeze . TextFormat::RED."You can not use commands while frozen.");
-				$event->setCancelled(true);
-				return true;
+		if($this->getConfig("commands-frozen") === false) {
+			if(strpos($message, "/") !== false) {
+				if(in_array($player->getName(), $this->frozen)) {
+					$player->addActionBarMessage(TextFormat::RED . "You are Frozen!");
+					$player->sendMessage($this->freeze . TextFormat::RED."You can not use commands while frozen.");
+					$event->setCancelled(true);
+					return true;
+				}
 			}
 		}
 	}
+	private function filterVar(String $str, $p) {
+		$configType = $this->getConfig()->get($str);
+		$fin = str_replace("%player%", $p->getName(), $configType);
+		return $fin;
+	}
+
 	public function onDisable() {
 		$this->getLogger()->info(TextFormat::RED . "Freeze Plugin Disabled");
 	}
